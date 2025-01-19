5dab8881b79707252de4bf6f74de5ec5bbf2801d
y-a-r-i-k <gusevyaroslaveggg666@gmail.com>
2025-01-19 21:53:57 +0300

    n1149 added

---
+++ n1149.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string n;
+    cin >> n;
+
+    cout << count(n.begin(), n.end(), '0');
+}
