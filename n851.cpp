6c58f98947ac256c7740e27989ddae496d39a420
y-a-r-i-k <gusevyaroslaveggg666@gmail.com>
2025-02-01 14:25:27 +0300

    n851 updated - second solution

--- n851.cpp
+++ n851.cpp
@@ -12,8 +12,17 @@ string solove(int nb, int ng)
         --nb;
         ng -= 2;
     }
+    // return string(ng, 'G') + s + string(nb, 'B'); //first version of solution
 
-    return string(ng, 'G') + s + string(nb, 'B');
+    // second solution
+    if (nb >= 1 && ng >= 1)
+    {
+        s += "BG";
+        --nb;
+        --ng;
+    }
+
+    return string(nb, 'B') + s + string(ng, 'G');
 }
 
 int main()
