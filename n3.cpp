f14aad9d465f054bfee609d40caf2160cb1397ab
y-a-r-i-k <gusevyaroslaveggg666@gmail.com>
2025-02-10 21:47:51 +0300

    n3 added

---
+++ n3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    long long n, a;
+    cin >> n;
+    
+    a = n / 10;
+
+    if (a * (a + 1) != 0) {
+        cout << a * (a + 1) << "25";
+    }else {
+        cout << "25";
+    }
+}
