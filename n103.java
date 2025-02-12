0df3d3773afac1ed5861bc899b4968cac4e3bb14
y-a-r-i-k <gusevyaroslaveggg666@gmail.com>
2025-02-12 21:13:33 +0300

    n103 - new java version added

---
+++ n103.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class n103 {
+    public static void main(String[] args) {
+        BigInteger a, b;
+        
+        Scanner in = new Scanner(System.in);
+        a = in.nextBigInteger();
+        b = in.nextBigInteger(); 
+
+        System.out.println(a.add(b));
+    }
+}
