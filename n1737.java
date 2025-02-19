e9106d60508558aca2b25a7ea80dee3f95811bb6
y-a-r-i-k <gusevyaroslaveggg666@gmail.com>
2025-02-19 20:16:54 +0300

    n1737 added but not soloved java

---
+++ n1737.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class n1737 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String s = in.nextLine();
+        int len = s.length();
+
+        BigInteger res = BigInteger.valueOf(1);
+        for (int i = 1; i <= len; ++i) {
+            res.multiply(BigInteger.valueOf(i));
+        }
+
+        System.out.println(res);
+
+        in.close();
+    }    
+}
