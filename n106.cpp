651003adb5df188915099640cb0c0181a9f2fe25
y-a-r-i-k <gusevyaroslaveggg666@gmail.com>
2025-02-26 21:35:01 +0300

    n106 added

---
+++ n106.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream input("INPUT.TXT");
+    ofstream output("OUTPUT.TXT");
+
+    int N;
+    input >> N;
+
+    int countH = 0; // Количество монеток с гербом (0)
+    int countT = 0; // Количество монеток с решкой (1)
+
+    // Подсчет монеток
+    for (int i = 0; i < N; ++i)
+    {
+        int coin;
+        input >> coin;
+        if (coin == 0)
+        {
+            countH++;
+        }
+        else
+        {
+            countT++;
+        }
+    }
+
+    // Минимальное количество переворотов
+    int minFlips = min(countH, countT);
+    output << minFlips << endl;
+
+    input.close();
+    output.close();
+
+    return 0;
+}
